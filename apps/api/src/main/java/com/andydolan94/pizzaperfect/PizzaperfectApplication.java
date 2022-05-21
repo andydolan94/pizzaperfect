0519732069be7bdb073f6cfac0f28df0fb063330
Andrew <andrew@dolan.co.nz>
2022-05-21 19:43:47 +1200

    initial commit

---
+++ apps/api/src/main/java/com/andydolan94/pizzaperfect/PizzaperfectApplication.java
@@ -0,0 +1,13 @@
+package com.andydolan94.pizzaperfect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaperfectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaperfectApplication.class, args);
+	}
+
+}
---
+++ apps/api/src/test/java/com/andydolan94/pizzaperfect/PizzaperfectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andydolan94.pizzaperfect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaperfectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
