8f92c6a20b6eaeb49f87202d284312a49e581aec
Andrew Dolan <andrew@dolan.co.nz>
2022-05-21 19:01:52 +1200

    initial commit

---
+++ apps/pizzaperfect/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pizzaperfect-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'pizzaperfect';
+}
