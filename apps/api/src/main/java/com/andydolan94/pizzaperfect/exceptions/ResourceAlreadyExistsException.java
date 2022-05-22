242b75d43b5d1c67ab851452d2523b05eb52ab50
Andrew <andrew@dolan.co.nz>
2022-05-22 14:32:39 +1200

    doc(api): updated javadoc for exceptions

--- apps/api/src/main/java/com/andydolan94/pizzaperfect/exceptions/ResourceAlreadyExistsException.java
+++ apps/api/src/main/java/com/andydolan94/pizzaperfect/exceptions/ResourceAlreadyExistsException.java
@@ -2,8 +2,15 @@ package com.andydolan94.pizzaperfect.exceptions;
 
 public class ResourceAlreadyExistsException extends Exception {
 
+	/**
+	 * Class constructor
+	 */
 	public ResourceAlreadyExistsException() {}
 
+	/**
+	 * Class constructor defining the message
+	 * @param msg the message to set
+	 */
 	public ResourceAlreadyExistsException(String msg) {
 		super(msg);
 	}
