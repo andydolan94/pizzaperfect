e0418f9d90fc201b34fe653c7cc68698c63f5e91
Andrew <andrew@dolan.co.nz>
2022-05-22 12:38:27 +1200

    feat(api): added resource exceptions - Bad resource exception - Resource already exists exception - Resource not found exception

---
+++ apps/api/src/main/java/com/andydolan94/pizzaperfect/exceptions/ResourceAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.andydolan94.pizzaperfect.exceptions;
+
+public class ResourceAlreadyExistsException extends Exception {
+
+	public ResourceAlreadyExistsException() {}
+
+	public ResourceAlreadyExistsException(String msg) {
+		super(msg);
+	}
+}
---
+++ apps/api/src/main/java/com/andydolan94/pizzaperfect/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.andydolan94.pizzaperfect.exceptions;
+
+public class ResourceNotFoundException extends Exception {
+
+	public ResourceNotFoundException() {}
+
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+}
