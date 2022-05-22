58052e847c4008e20f033e489e4ce31e206b707e
Andrew <andrew@dolan.co.nz>
2022-05-22 14:39:09 +1200

    doc(api): added pizza order service javadoc

--- apps/api/src/main/java/com/andydolan94/pizzaperfect/services/PizzaOrderService.java
+++ apps/api/src/main/java/com/andydolan94/pizzaperfect/services/PizzaOrderService.java
@@ -15,30 +15,56 @@ public class PizzaOrderService {
 
 	private PizzaOrderRepository pizzaOrderRepository;
 
+	/**
+	 * Checks if an order exists in the database
+	 * @param id the id of the order
+	 * @return the boolean indicating if the order exists or not
+	 */
 	private boolean existsById(Long id) {
 		return pizzaOrderRepository.existsById(id);
 	}
 
+	/**
+	 * Class constructor defining its associated repository
+	 * @param pizzaRepository the repository
+	 */
 	public PizzaOrderService(PizzaOrderRepository pizzaOrderRepository) {
 		this.pizzaOrderRepository = pizzaOrderRepository;
 	}
 
+	/**
+	 * Lists all of the orders in the database
+	 * @return the list of orders
+	 */
 	public List<PizzaOrder> findAll() {
 		return pizzaOrderRepository.findAll();
 	}
 
-	public Optional<PizzaOrder> findById(long id) throws ResourceNotFoundException {
+	/**
+	 * Finds an order given a supplied id
+	 * @param id the id of the order
+	 * @return the order
+	 * @throws ResourceNotFoundException if the order cannot be found
+	 */
+	public Optional<PizzaOrder> findById(long id)
+		throws ResourceNotFoundException {
 		if (!existsById(id)) {
 			throw new ResourceNotFoundException(
-				"Cannot find Contact with id: %d".formatted(
-						id
-					)
+				"Cannot find Contact with id: %d".formatted(id)
 			);
 		} else {
 			return pizzaOrderRepository.findById(id);
 		}
 	}
 
+	/**
+	 * Saves an order in the database
+	 * @param pizzaOrder the order to save
+	 * @return the saved order
+	 * @throws BadResourceException if the order given is malformed
+	 * @throws ResourceAlreadyExistsException if the order given already exists
+	 * 										  with the supplied id
+	 */
 	public PizzaOrder save(PizzaOrder pizzaOrder)
 		throws BadResourceException, ResourceAlreadyExistsException {
 		if (
@@ -62,6 +88,13 @@ public class PizzaOrderService {
 		}
 	}
 
+	/**
+	 * Updates an existing order with the matching id
+	 * @param pizzaOrder the order to be updated
+	 * @return the updated order
+	 * @throws BadResourceException if the updated order is malformed
+	 * @throws ResourceNotFoundException if the existing order cannot be found
+	 */
 	public PizzaOrder update(PizzaOrder pizzaOrder)
 		throws BadResourceException, ResourceNotFoundException {
 		if (
