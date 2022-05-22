14a342eccaf1ccefa3b2f421489d7bbc7706ac7e
Andrew <andrew@dolan.co.nz>
2022-05-22 14:58:27 +1200

    feat(api): added pizza controller

---
+++ apps/api/src/main/java/com/andydolan94/pizzaperfect/controllers/PizzaController.java
@@ -0,0 +1,91 @@
+package com.andydolan94.pizzaperfect.controllers;
+
+import com.andydolan94.pizzaperfect.entities.Pizza;
+import com.andydolan94.pizzaperfect.exceptions.BadResourceException;
+import com.andydolan94.pizzaperfect.exceptions.ResourceAlreadyExistsException;
+import com.andydolan94.pizzaperfect.exceptions.ResourceNotFoundException;
+import com.andydolan94.pizzaperfect.services.PizzaService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PizzaController {
+	
+	@Autowired
+	private PizzaService pizzaService;
+
+	/**
+	 * Gets a list of pizzas
+	 * @return a response with the list of pizzas
+	 */
+	@GetMapping("/pizzas")
+	public ResponseEntity<List<Pizza>> getAllPizza() {
+		return new ResponseEntity<>(pizzaService.findAll(), HttpStatus.OK);
+	}
+
+	/**
+	 * Gets a single pizza
+	 * @param id the pizza id
+	 * @return a response with the pizza
+	 */
+	@GetMapping("/pizzas/{id}")
+	public ResponseEntity<Optional<Pizza>> getById(@PathVariable Long id) {
+		try {
+			return new ResponseEntity<>(
+				pizzaService.findById(id),
+				HttpStatus.OK
+			);
+		} catch (ResourceNotFoundException ex) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	/**
+	 * Creates a pizza
+	 * @param pizza the pizza to be created
+	 * @return a response with the pizza
+	 */
+	@PostMapping("/pizzas")
+	public ResponseEntity<Pizza> create(
+		@RequestBody Pizza pizza
+	) {
+		try {
+			return new ResponseEntity<>(
+				pizzaService.save(pizza),
+				HttpStatus.CREATED
+			);
+		} catch (ResourceAlreadyExistsException ex) {
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		} catch (BadResourceException ex) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	/**
+	 * Updates a pizza
+	 * @param id the id of the existing pizza
+	 * @param pizza the pizza to replace the existing pizza
+	 * @return a response with the updated pizza
+	 */
+	@PutMapping("/pizzas/{id}")
+	public ResponseEntity<Pizza> update(
+		@PathVariable Long id,
+		@RequestBody Pizza pizza
+	) {
+		try {
+			pizza.setId(id); // Overwrite the id if one is supplied in the body
+			return new ResponseEntity<>(
+				pizzaService.update(pizza),
+				HttpStatus.OK
+			);
+		} catch (ResourceNotFoundException ex) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} catch (BadResourceException ex) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+}
