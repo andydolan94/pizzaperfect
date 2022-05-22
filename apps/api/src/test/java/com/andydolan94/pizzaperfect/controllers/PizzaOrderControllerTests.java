327f222a97d877e531570503a340923c38d36fa3
Andrew <andrew@dolan.co.nz>
2022-05-22 12:34:24 +1200

    test(api): added pizza order controller tests

---
+++ apps/api/src/test/java/com/andydolan94/pizzaperfect/controllers/PizzaOrderControllerTests.java
@@ -0,0 +1,165 @@
+package com.andydolan94.pizzaperfect.controllers;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.andydolan94.pizzaperfect.entities.PizzaOrder;
+import com.andydolan94.pizzaperfect.services.PizzaOrderService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class PizzaOrderControllerTests {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@MockBean
+	private PizzaOrderService pizzaOrderService;
+
+	@Test
+	void shouldGetAllThePizzaOrders() throws Exception {
+		List<PizzaOrder> pizzaOrderList = new ArrayList<>();
+		pizzaOrderList.add(
+			new PizzaOrder(
+				1L,
+				"John Doe",
+				"1 example lane, testown, presetville 1234"
+			)
+		);
+		pizzaOrderList.add(
+			new PizzaOrder(
+				2L,
+				"Random Citizen",
+				"2 interesting drive, upward, backward 2468"
+			)
+		);
+
+		when(pizzaOrderService.findAll()).thenReturn(pizzaOrderList);
+
+		mockMvc
+			.perform(
+				MockMvcRequestBuilders
+					.get("/pizza-orders")
+					.contentType(MediaType.APPLICATION_JSON)
+			)
+			.andExpect(jsonPath("$", hasSize(2)))
+			.andDo(print());
+	}
+
+	@Test
+	void shouldGetOnePizzaOrder() throws Exception {
+		long id = 1L;
+
+		PizzaOrder pizzaOrder = new PizzaOrder(
+			id,
+			"John Doe",
+			"1 example lane, testown, presetville 1234"
+		);
+
+		when(pizzaOrderService.findById(id))
+			.thenReturn(Optional.of(pizzaOrder));
+
+		mockMvc
+			.perform(MockMvcRequestBuilders.get("/pizza-orders/{id}", id))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.id").value(id))
+			.andExpect(
+				jsonPath("$.customerName").value(pizzaOrder.getCustomerName())
+			)
+			.andExpect(
+				jsonPath("$.deliveryAddress")
+					.value(pizzaOrder.getDeliveryAddress())
+			)
+			.andDo(print());
+	}
+
+	@Test
+	void shouldSuccessfullyCreateAPizzaOrder() throws Exception {
+		long id = 1L;
+		PizzaOrder pizzaOrder = new PizzaOrder(
+			id,
+			"John Doe",
+			"1 example lane, testown, presetville 1234"
+		);
+
+		when(pizzaOrderService.save(any(PizzaOrder.class)))
+			.thenReturn(pizzaOrder);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		String pizzaOrderJSON = objectMapper.writeValueAsString(pizzaOrder);
+
+		ResultActions result = mockMvc.perform(
+			post("/pizza-orders")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(pizzaOrderJSON)
+		);
+
+		result
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("$.id").value(id))
+			.andExpect(jsonPath("$.customerName").value("John Doe"))
+			.andExpect(
+				jsonPath("$.deliveryAddress")
+					.value("1 example lane, testown, presetville 1234")
+			);
+	}
+
+	@Test
+	void shouldSuccessfullyUpdateAPizzaOrder() throws Exception {
+		long id = 1L;
+		PizzaOrder pizzaOrder = new PizzaOrder(
+			id,
+			"John Doe",
+			"1 example lane, testown, presetville 1234"
+		);
+		PizzaOrder newPizzaOrder = new PizzaOrder(
+			id,
+			"Random Citizen",
+			"2 interesting drive, upward, backward 2468"
+		);
+
+		when(pizzaOrderService.findById(id))
+			.thenReturn(Optional.of(pizzaOrder));
+		when(pizzaOrderService.update(any(PizzaOrder.class)))
+			.thenReturn(newPizzaOrder);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		String newPizzaOrderJSON = objectMapper.writeValueAsString(
+			newPizzaOrder
+		);
+
+		ResultActions result = mockMvc.perform(
+			put("/pizza-orders/{id}", id)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(newPizzaOrderJSON)
+		);
+
+		result
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.customerName").value("Random Citizen"))
+			.andExpect(
+				jsonPath("$.deliveryAddress")
+					.value("2 interesting drive, upward, backward 2468")
+			);
+	}
+}
