7710a8a9999b7a9724f79cddd39b5624f3cd044d
Andrew <andrew@dolan.co.nz>
2022-05-22 12:37:32 +1200

    feat(api): added pizza order repository

---
+++ apps/api/src/main/java/com/andydolan94/pizzaperfect/repositories/PizzaOrderRepository.java
@@ -0,0 +1,8 @@
+package com.andydolan94.pizzaperfect.repositories;
+
+import com.andydolan94.pizzaperfect.entities.PizzaOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("pizzaOrderRepository")
+public interface PizzaOrderRepository extends JpaRepository<PizzaOrder, Long> {}
