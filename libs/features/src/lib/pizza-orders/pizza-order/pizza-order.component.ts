01b32aec5949a7b70672276dbc8556da2200b3ce
Andrew <andrew@dolan.co.nz>
2022-05-26 20:00:57 +1200

    feat(feature): implemented pizza order function - can now mark off as delivered - can now view all of the orders - made it look snazzy

--- libs/features/src/lib/pizza-orders/pizza-order/pizza-order.component.ts
+++ libs/features/src/lib/pizza-orders/pizza-order/pizza-order.component.ts
@@ -1,12 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { PizzaOrderService } from '@pizzaperfect/core';
+import { PizzaOrder } from '@pizzaperfect/data';
 
 @Component({
 	selector: 'pizzaperfect-pizza-order',
 	templateUrl: './pizza-order.component.html',
 	styleUrls: ['./pizza-order.component.scss'],
 })
-export class PizzaOrderComponent implements OnInit {
-	constructor() {}
+export class PizzaOrderComponent {
+	@Input() pizzaOrder!: PizzaOrder;
+	@Output() updateRequest = new EventEmitter<void>();
 
-	ngOnInit(): void {}
+	constructor(private pizzaOrderService: PizzaOrderService, private snackBar: MatSnackBar) {}
+
+	onMarkAsDelivered(pizzaOrder: PizzaOrder) {
+		this.pizzaOrderService.updatePizzaOrder(pizzaOrder.id, {
+			...pizzaOrder,
+			delivered: true,
+		}).subscribe({
+			next: (pizzaOrder: PizzaOrder) => {
+				this.snackBar.open(`🍕 Order marked as delivered! ${pizzaOrder.customerName} will be happy`, 'close', {
+					duration: 3000,
+				});
+				this.updateRequest.emit();
+			},
+			error: (err: HttpErrorResponse) => {
+				console.error(err)
+				this.snackBar.open(
+					`⚠️ Updating pizza order failed, please try again later`,
+					'close',
+					{ duration: 3000 }
+				);
+			}
+		});		
+	}
 }
